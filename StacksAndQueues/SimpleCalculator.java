87216586e15ec10230ef5414de0d0d28072258f9
Konstantin Dimitrov <1konstantin.dimitrov@gmail.com>
2022-07-03 19:13:57 +0300

    Added new exercise.

--- StacksAndQueues/SimpleCalculator.java
+++ StacksAndQueues/SimpleCalculator.java
@@ -1,2 +1,28 @@
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class SimpleCalculator {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        ArrayDeque<String> stack = new ArrayDeque<>(Arrays.asList(scanner.nextLine().split("\\s+")));
+
+        while (stack.size()>1) {
+            int left = Integer.parseInt(stack.pop());
+            String operation = stack.pop();
+            int right = Integer.parseInt(stack.pop());
+            int sum;
+            if (operation.equals("+")) {
+                sum = left + right;
+            } else {
+                sum = left - right;
+            }
+            stack.push(String.valueOf(sum));
+        }
+
+        System.out.println(stack.peek());
+
+    }
 }
