cb0a8337c8022b07b911e929c18f4dcaef56acd1
Konstantin Dimitrov <1konstantin.dimitrov@gmail.com>
2022-07-05 17:42:02 +0300

    Added new exercise.

--- StacksAndQueues/DecimalToBinary.java
+++ StacksAndQueues/DecimalToBinary.java
@@ -1,2 +1,24 @@
+import java.util.ArrayDeque;
+import java.util.Scanner;
+
 public class DecimalToBinary {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        ArrayDeque<Integer> stack = new ArrayDeque<>();
+
+        int decimalNumber = Integer.parseInt(scanner.nextLine());
+
+        if (decimalNumber == 0) {
+            System.out.println(0);
+            return;
+        }
+
+        while (decimalNumber != 0) {
+            stack.push(decimalNumber % 2);
+            decimalNumber /= 2;
+        }
+
+        stack.forEach(System.out::print);
+    }
 }
