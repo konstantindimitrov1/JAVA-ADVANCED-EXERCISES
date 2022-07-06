0537769a2173b92ddcbddf2daea6e0dfbae58c0e
Konstantin Dimitrov <1konstantin.dimitrov@gmail.com>
2022-07-06 20:29:06 +0300

    Added new exercise.

--- StacksAndQueues/MatchingBrackets.java
+++ StacksAndQueues/MatchingBrackets.java
@@ -1,2 +1,23 @@
+import java.util.ArrayDeque;
+import java.util.Scanner;
+
 public class MatchingBrackets {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        ArrayDeque<Integer> stack = new ArrayDeque<>();
+
+        String expression = scanner.nextLine();
+
+        for (int i = 0; i < expression.length(); i++) {
+            if (expression.charAt(i) == '(') {
+                stack.push(i);
+            } else if (expression.charAt(i) == ')') {
+                int startIndex = stack.pop();
+                String contents = expression.substring(startIndex, i+1);
+                System.out.println(contents);
+            }
+        }
+
+    }
 }
