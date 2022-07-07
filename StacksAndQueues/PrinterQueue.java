32451b6a188cc102d796aae564aa883419b01820
Konstantin Dimitrov <1konstantin.dimitrov@gmail.com>
2022-07-07 17:41:09 +0300

    Added new exercise.

--- StacksAndQueues/PrinterQueue.java
+++ StacksAndQueues/PrinterQueue.java
@@ -1,2 +1,31 @@
+import java.util.ArrayDeque;
+import java.util.Scanner;
+
 public class PrinterQueue {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        ArrayDeque<String> queue = new ArrayDeque<>();
+
+        String input = scanner.nextLine();
+
+        while (!input.equals("print")) {
+            if (input.equals("cancel")) {
+                if (queue.isEmpty()) {
+                    System.out.println("Printer is on standby");
+                } else {
+                    String output = "Canceled " + queue.pollFirst();
+                    System.out.println(output);
+                }
+            } else {
+                queue.offer(input);
+            }
+
+            input = scanner.nextLine();
+        }
+
+        for (String s : queue) {
+            System.out.println(s);
+        }
+    }
 }
