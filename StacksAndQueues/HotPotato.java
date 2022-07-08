25e38e56625c587cb9eb4f50ecc73474a7e947df
Konstantin Dimitrov <1konstantin.dimitrov@gmail.com>
2022-07-08 17:26:13 +0300

    Added new exercise.

--- StacksAndQueues/HotPotato.java
+++ StacksAndQueues/HotPotato.java
@@ -1,2 +1,25 @@
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class HotPotato {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String kids = scanner.nextLine();
+        int n = Integer.parseInt(scanner.nextLine());
+
+        ArrayDeque<String> queue = new ArrayDeque<>();
+
+        Arrays.stream(kids.split("\\s+")).forEach(queue::offer);
+
+        while (queue.size() > 1) {
+            for (int i = 1; i < n; i++) {
+                queue.offer(queue.poll());
+            }
+            System.out.println("Removed " + queue.poll());
+        }
+
+        System.out.println("Last is " + queue.poll());
+    }
 }
