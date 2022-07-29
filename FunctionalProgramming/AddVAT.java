9e702c7cc6665b94a02a4151f36153b3163f314e
Konstantin Dimitrov <1konstantin.dimitrov@gmail.com>
2022-07-29 14:05:25 +0300

    Added new exercise.

---
+++ FunctionalProgramming/AddVAT.java
@@ -0,0 +1,21 @@
+package FunctionalProgramming;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.function.UnaryOperator;
+
+public class AddVAT {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        UnaryOperator<Double> priceWithVAT = e -> e * 1.2;
+
+
+        List<Double> prices = Arrays.stream(scanner.nextLine().split(", "))
+                .map(Double::parseDouble).toList();
+
+        System.out.println("Prices with VAT:");
+        prices.forEach(e -> System.out.printf("%.2f\n", priceWithVAT.apply(e)));
+    }
+}
