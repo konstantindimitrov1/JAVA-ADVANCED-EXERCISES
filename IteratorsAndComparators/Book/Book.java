ccc73dcd63ba04e1b67853b3752449eca1ff9348
Konstantin Dimitrov <1konstantin.dimitrov@gmail.com>
2022-09-07 12:18:12 +0300

    Added new exercise.

---
+++ IteratorsAndComparators/Book/Book.java
@@ -0,0 +1,41 @@
+package IteratorsAndComparators.Book;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Book {
+    private String title;
+    private int year;
+    private List<String> authors;
+
+    public Book(String name, int year, String... authors) {
+        this.setTitle(name);
+        this.setYear(year);
+        this.setAuthors(authors);
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    private void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    private void setYear(int year) {
+        this.year = year;
+    }
+
+    private void setAuthors(String... authors) {
+        this.authors = Arrays.stream(authors).collect(Collectors.toList());
+    }
+
+    public List<String> getAuthors() {
+        return authors;
+    }
+}
